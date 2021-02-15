e29a88c778cfc801a8c0a882304dcad426c67dcd
Josias Alvarado <josiasjag@gmail.com>
2021-02-15 14:23:44 -0600

    Update models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
   
-  has_many :groups
-  has_many :expenses
+  has_many :groups, dependent: :destroy
+  has_many :expenses, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_200830) do
+ActiveRecord::Schema.define(version: 2021_02_15_201338) do
 
   create_table "expenses", force: :cascade do |t|
     t.string "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_200830) do
     t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
