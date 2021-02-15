578baa2ca623b4cecc0566c45b5640b8dd58137f
Josias Alvarado <josiasjag@gmail.com>
2021-02-15 15:36:47 -0600

    Update routes

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,10 @@
 class ExpensesController < ApplicationController
+
+  def index
+    redirect_to root_path
+  end
+
+  def show
+    @expense = Expense.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users do
     resources :groups
-    resources :expenses
+    resources :expenses, only: :show
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
