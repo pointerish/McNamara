35fde6a46f65e556d2075080bef0ada48a1f6190
Josias Alvarado <josiasjag@gmail.com>
2021-02-15 15:23:31 -0600

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import  "../layouts/application.css";
 
+const images = require.context('.../images', true)
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'user#show'
+  root 'users#show'
 
   devise_for :users
   resources :users do
