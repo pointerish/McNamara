684051a44795f905bd16c91593e8852d99b4e3c5
Josias Alvarado <josiasjag@gmail.com>
2021-02-17 13:29:38 -0600

    fix bug where logout functionality was not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery_ujs
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import  "../layouts/application.css";
-
-const images = require.context('.../images', true)
 
 Rails.start()
 Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#show'
-
+  
   devise_for :users
   resources :users do
     resources :groups
