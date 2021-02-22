4d64c6b3713907f8916c05298b1234aa1b897d5e
Josias Alvarado <josiasjag@gmail.com>
2021-02-22 10:40:30 -0600

    Fix Rubocop errors

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  let(:user) { User.new(username:'anerlo', email:'anerlo@gmail.com', password:'123123') }
-  let(:group) { Group.new(name:"Expense General", description: "An expense category", user_id: 1) }
+  let(:user) { User.new(username: 'anerlo', email: 'anerlo@gmail.com', password: '123123') }
+  let(:group) { Group.new(name: 'Expense General', description: 'An expense category', user_id: 1) }
   it 'is not valid without attributes' do
     expect(Expense.new).to_not be_valid
   end
   it 'is not valid without user_id' do
-    expect(Expense.new(name: 'YouTube', amount: 2, group_id:1)).to_not be_valid
+    expect(Expense.new(name: 'YouTube', amount: 2, group_id: 1)).to_not be_valid
   end
   it 'is not valid without name' do
-    expect(Expense.new(amount: 2, group_id:group.id, user_id: user.id)).to_not be_valid
+    expect(Expense.new(amount: 2, group_id: group.id, user_id: user.id)).to_not be_valid
   end
   it 'is not valid without amount' do
-    expect(Expense.new(name: 'YouTube', group_id:group.id, user_id: user.id)).to_not be_valid
+    expect(Expense.new(name: 'YouTube', group_id: group.id, user_id: user.id)).to_not be_valid
   end
   it 'is not valid without group_id' do
     expect(Expense.new(amount: 2, name: 'YouTube', user_id: user.id)).to_not be_valid
