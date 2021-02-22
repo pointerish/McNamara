35d69acbe94f7d789f194fddbc48a196e0670ff9
Josias Alvarado <josiasjag@gmail.com>
2021-02-22 10:38:02 -0600

    Update test for Expense model

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -21,4 +21,10 @@ RSpec.describe Expense, type: :model do
   it 'is not valid without group_id' do
     expect(Expense.new(amount: 2, name: 'YouTube', user_id: user.id)).to_not be_valid
   end
+  it 'is not valid with name less than 2 characters' do
+    expect(Expense.new(amount: 2, name: 'Y', user_id: user.id)).to_not be_valid
+  end
+  it 'is not valid with name greater than 20 characters' do
+    expect(Expense.new(amount: 2, name: '123456789012345678901', user_id: user.id)).to_not be_valid
+  end
 end
