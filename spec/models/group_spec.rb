dd3016d72a271117b55c79229b5fa11b6b38d157
Josias Alvarado <josiasjag@gmail.com>
2021-02-25 14:46:05 -0600

    Update group_spec.rb

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,11 +4,9 @@ RSpec.describe Group, type: :model do
   it 'is not valid without attributes' do
     expect(Group.new).to_not be_valid
   end
-
   it 'is not valid without name' do
     expect(Group.new(description: 'Test!')).to_not be_valid
   end
-
   it 'is not valid without description' do
     expect(Group.new(name: 'Test!')).to_not be_valid
   end
